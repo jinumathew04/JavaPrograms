b463467436593f46cfac8f8dc671fdc60de24109
Joe Tom Job <joetomjob@Joes-MacBook-Pro.local>
2020-05-21 19:35:25 -0700

    BFS graph

--- src/DataStructures/BFSNode.java
+++ src/DataStructures/BFSNode.java
@@ -2,6 +2,8 @@ package DataStructures;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BFSNode {
     String item;
@@ -14,28 +16,41 @@ public class BFSNode {
     public void addNeighbour(BFSNode node) {
         neighbours.add(node);
     }
+
     public static boolean chkPathExist(BFSNode start,BFSNode end){
         HashSet<BFSNode> visited = new HashSet<>();
         return chkPathExist(start,end,visited);
     }
 
     public static boolean chkPathExist(BFSNode start, BFSNode end,HashSet<BFSNode> visited) {
-        if (start == end) {
-            return true;
+        Queue<BFSNode> q = new LinkedList<>();
+        q.add(start);
+        visited.add(start);
+        while(!q.isEmpty()) {
+            BFSNode b = q.poll();
+            if (b == end) {
+                return true;
+            }
+            for (BFSNode s : b.neighbours) {
+                if (!visited.contains(s)) {
+                    q.add(s);
+                    visited.add(s);
+                }
+            }
         }
-        queue<
+        return false;
     }
 
     public static void main(String[] args) {
-        DFSNode a = new DFSNode("A");
-        DFSNode b = new DFSNode("B");
-        DFSNode c = new DFSNode("C");
-        DFSNode d = new DFSNode("D");
-        DFSNode e = new DFSNode("E");
-        DFSNode f = new DFSNode("F");
-        DFSNode g = new DFSNode("G");
-        DFSNode h = new DFSNode("H");
-        DFSNode j = new DFSNode("J");
+        BFSNode a = new BFSNode("A");
+        BFSNode b = new BFSNode("B");
+        BFSNode c = new BFSNode("C");
+        BFSNode d = new BFSNode("D");
+        BFSNode e = new BFSNode("E");
+        BFSNode f = new BFSNode("F");
+        BFSNode g = new BFSNode("G");
+        BFSNode h = new BFSNode("H");
+        BFSNode i = new BFSNode("I");
 
         a.addNeighbour(b);
         a.addNeighbour(c);
@@ -46,6 +61,8 @@ public class BFSNode {
         d.addNeighbour(g);
         d.addNeighbour(h);
 
+        System.out.println(chkPathExist(a,i));
+        System.out.println(chkPathExist(a,g));
 
     }
 }
